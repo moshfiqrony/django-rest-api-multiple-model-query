16b729559c9a074b0ffa21319802516f35cc765e
moshfiqrony <moshfiqrony@gmail.com>
2019-02-21 14:50:27 +0600

    table added and search not fixed

--- frontend/src/ShowData.js
+++ frontend/src/ShowData.js
@@ -1,39 +1,83 @@
 import React from 'react'
-import { Table, } from 'antd';
 
-const columns = [{
-  title: 'Campaign Name',
-  dataIndex: 'campaignId.name',
-  key: 'name',
-  render: text => <a href="javascript:;">{text}</a>,
-},{
-  title: 'CL Name',
-  dataIndex: 'clId.name',
-  key: 'clname',
-},{
-  title: 'CL district',
-  dataIndex: 'clId.district.name',
-  key: 'cldistrict',
-},{
-  title: 'CL Phone Number',
-  dataIndex: 'clId.phone',
-  key: 'clphone',
-},{
-  title: 'CL Name',
-  dataIndex: 'agentId.name',
-  key: 'clname',
-},{
-  title: 'CL district',
-  dataIndex: 'agentId.district.name',
-  key: 'cldistrict',
-},{
-  title: 'CL Phone Number',
-  dataIndex: 'agentId.phone',
-  key: 'clphone',
-}];
+import {
+  Table, Input, Button, Icon,
+} from 'antd';
+import Highlighter from 'react-highlight-words';
 
+class App extends React.Component {
+  state = {
+    searchText: '',
+  };
 
+  getColumnSearchProps = (dataIndex) => ({
+    filterDropdown: ({
+      setSelectedKeys, selectedKeys, confirm, clearFilters,
+    }) => (
+      <div style={{ padding: 8 }}>
+        <Input
+          ref={node => { this.searchInput = node; }}
+          placeholder={`Search ${dataIndex}`}
+          value={selectedKeys[0]}
+          onChange={e => setSelectedKeys(e.target.value ? [e.target.value] : [])}
+          onPressEnter={() => this.handleSearch(selectedKeys, confirm)}
+          style={{ width: 188, marginBottom: 8, display: 'block' }}
+        />
+        <Button
+          type="primary"
+          onClick={() => this.handleSearch(selectedKeys, confirm)}
+          icon="search"
+          size="small"
+          style={{ width: 90, marginRight: 8 }}
+        >
+          Search
+        </Button>
+        <Button
+          onClick={() => this.handleReset(clearFilters)}
+          size="small"
+          style={{ width: 90 }}
+        >
+          Reset
+        </Button>
+      </div>
+    ),
+    filterIcon: filtered => <Icon type="search" style={{ color: filtered ? '#1890ff' : undefined }} />,
+    // onFilter: (value, record) => record[dataIndex].toString().toLowerCase().includes(value.toLowerCase()),
+    onFilterDropdownVisibleChange: (visible) => {
+      if (visible) {
+        setTimeout(() => this.searchInput.select());
+      }
+    },
+    render: (text) => (
+      <Highlighter
+        highlightStyle={{ backgroundColor: '#ffc069', padding: 0 }}
+        searchWords={[this.state.searchText]}
+        autoEscape
+        // textToHighlight={text.toString()}
+      />
+    ),
+  })
 
-export default function (props) {
-    return(<Table columns={columns} dataSource={props.data} />)
+  handleSearch = (selectedKeys, confirm) => {
+    confirm();
+    this.setState({ searchText: selectedKeys[0] });
+  }
+
+  handleReset = (clearFilters) => {
+    clearFilters();
+    this.setState({ searchText: '' });
+  }
+
+  render() {
+    const columns = [{
+      title: 'Name',
+      dataIndex: 'clId.name',
+      key: 'name',
+      width: '30%',
+      ...this.getColumnSearchProps('name'),
+    }];
+    return <Table columns={columns} dataSource={this.props.data} />;
+  }
 }
+
+export default App;
